93ccf8abb2cce90e73dfab57e9127d6b52bd1918
Wtorres <wtorres@cads.com.gt>
2019-08-20 18:03:43 -0600

    Ajustes en visualizacion de cursos

--- src/app/pages/movimientosAlumnos/cursos/cursos.component.ts
+++ src/app/pages/movimientosAlumnos/cursos/cursos.component.ts
@@ -10,44 +10,123 @@ import { AlumnoService } from '../alumno.service';
 })
 export class CursosComponent implements OnInit {
   seleccion: string = '';
-  objeto: any = {}
+  objeto: any = {};
+  objBreadcrumb: any = {};
+  unidad: number;
+  leccion: number;
+  contenido: number;
+  seleccionado: any;
+  seleccionContenido: any;
 
 
   constructor(@Inject(DOCUMENT) private _document, private alumno: AlumnoService) {
     alumno.inicializar();
+    this.unidad = 0;
+    this.leccion = 0;
+    this.contenido = 0;
   }
 
   ngOnInit() {
     this.initObj();
+  }
+
+  marcar(raiz: any, marcado: any, obj: any, unidad: any, leccion: any) {
+    this.seleccionado = raiz + marcado;
+    this.objBreadcrumb = obj;
+    this.unidad = unidad;
+    this.leccion = leccion;
+  }
+
+  siguiente() {
+    if ((this.leccion + 1) < this.objeto.contenido[this.unidad].subTemas.length) {
+      if (this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle.length === (this.contenido + 1)) {
+        this.objeto.contenido[this.unidad].subTemas[this.leccion].visto = true;
+        this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle[this.contenido].visto = true;
+        this.leccion = this.leccion + 1;
+        this.contenido = 0
+        this.leccionActual();
+      } else {
+        this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle[this.contenido].visto = true;
+        this.contenido = this.contenido + 1;
+        this.mostrar(this.contenido);
+
+      }
+
+    } else if (this.objeto.contenido[this.unidad].subTemas.length === (this.leccion + 1)) {
+      if (((this.leccion + 1) === this.objeto.contenido[this.unidad].subTemas.length) && ((this.unidad + 1) === this.objeto.contenido.length)) {
+        this.objeto.contenido[this.unidad].subTemas[this.leccion].visto = true;
+        this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle[this.contenido].visto = true;
+        console.log('llego al limite de cursos')
+        return;
+      }
+      if (this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle.length === (this.contenido + 1)) {
+        this.objeto.contenido[this.unidad].subTemas[this.leccion].visto = true;
+        this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle[this.contenido].visto = true;
+        this.unidad = this.unidad + 1
+        this.leccion = 0
+        this.contenido = 0;
+        this.leccionActual();
+      } else {
+        this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle[this.contenido].visto = true;
+        this.contenido = this.contenido + 1;
+        this.mostrar(this.contenido);
+      }
 
 
+    }
+  }
+
+  anterior() {
+    if (this.leccion === 0) {
+      if (this.leccion === 0 && this.unidad === 0 && this.contenido === 0) {
+        console.log('llego al inicio de los cursos')
+        return;
+      } else if (this.contenido !== 0){
+        this.contenido = this.contenido - 1
+        this.mostrar(this.contenido);
+        return;
+      }
+      if (this.contenido === 0) {
+        this.unidad = this.unidad - 1
+        this.leccion = (this.objeto.contenido[this.unidad].subTemas.length - 1);
+        this.contenido = this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle.length;
+        this.leccionActual();
+      } else {
+        this.contenido = this.contenido - 1
+        this.mostrar(this.contenido);
+      }
+
+    } else if (this.objeto.contenido[this.unidad].subTemas.length >= (this.leccion + 1)) {
+      if (this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle[this.contenido].length >= (this.contenido + 1)) {
+        this.contenido = this.contenido - 1
+        this.mostrar(this.contenido);
+      } else {
+        this.leccion = this.leccion - 1;
+        this.leccionActual();
+      }
+
+
+    }
+
+  }
+
+  leccionActual() {
+    this.seleccionado = this.objeto.contenido[this.unidad].refNombre +
+      this.objeto.contenido[this.unidad].subTemas[this.leccion].codigoContenido
+    this.mostrar(this.contenido);
   }
 
   ngOnDestroy() {
     this.alumno.destruir();
   }
 
-  mostrar(item: number) {
+  mostrar(item: number = 0) {
     let recuadro = document.getElementById("#recuadro");
-    switch (item) {
-      case 1:
-        this.seleccion = 'https://www.youtube.com/embed/zpOULjyy-n8?rel=0'
-        this._document.getElementById('recuadro').setAttribute('src', this.seleccion);
-        break;
-      case 2:
-        this.seleccion = 'https://camsolutionsgt.blogspot.com/'
-        this._document.getElementById('recuadro').setAttribute('src', this.seleccion);
-        break;
-      case 3:
-        this.seleccion = 'https://www.um.es/docencia/barzana/DAWEB/Lenguaje-de-programacion-JavaScript-1.pdf'
-        this._document.getElementById('recuadro').setAttribute('src', this.seleccion);
-        break;
+    this.seleccionContenido = item;
+    //this.seleccion = this.objeto.contenido[this.unidad].subTemas[this.leccion].videoUrlContenido;
+    this.seleccion = this.objeto.contenido[this.unidad].subTemas[this.leccion].detalle[item].url;
+    this._document.getElementById('recuadro').setAttribute('src', this.seleccion);
 
-      default:
-        this.seleccion = 'https://www.lawebdelprogramador.com/pdf/files/1532804764_manual-javascript-19.jpg'
-        this._document.getElementById('recuadro').setAttribute('src', this.seleccion);
-        break;
-    }
   }
 
   initObj() {
@@ -63,7 +142,25 @@ export class CursosComponent implements OnInit {
         subTemas: [{
           codigoContenido: 1,
           nombreContenido: "Preface",
-          videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI"
+          videoUrlContenido: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0",
+          detalle: [
+            {
+              url: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", tipo: 0, visto: false
+            },
+            {
+              url: "https://www.um.es/docencia/barzana/DAWEB/Lenguaje-de-programacion-JavaScript-1.pdf", tipo: 1, visto: false
+            },
+            {
+              url: "https://camsolutionsgt.blogspot.com/", tipo: 2, visto: false
+            },
+            {
+              url: "https://www.lawebdelprogramador.com/pdf/files/1532804764_manual-javascript-19.jpg", tipo: 3, visto: false
+            },
+            {
+              url: "https://www.youtube.com/watch?v=Sj-KJdE1bNY", tipo: 0, visto: false
+            },
+          ],
+          visto: false
         }],
         videoUrlContenido: ""
       },
@@ -76,22 +173,61 @@ export class CursosComponent implements OnInit {
         subTemas: [{
           codigoContenido: 1,
           nombreContenido: "Callbacks",
-          videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI"
+          videoUrlContenido: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0",
+          visto: false,
+          detalle: [
+            {
+              url: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", tipo: 0, visto: false
+            },
+            {
+              url: "https://www.um.es/docencia/barzana/DAWEB/Lenguaje-de-programacion-JavaScript-1.pdf", tipo: 1, visto: false
+            },
+            {
+              url: "https://camsolutionsgt.blogspot.com/", tipo: 2, visto: false
+            },
+            {
+              url: "https://www.lawebdelprogramador.com/pdf/files/1532804764_manual-javascript-19.jpg", tipo: 3, visto: false
+            },
+            {
+              url: "https://www.youtube.com/watch?v=Sj-KJdE1bNY", tipo: 0, visto: false
+            },
+          ],
+
         },
         {
           codigoContenido: 2,
           nombreContenido: "Asynchronous JavaScript",
-          videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI"
+          videoUrlContenido: "https://www.um.es/docencia/barzana/DAWEB/Lenguaje-de-programacion-JavaScript-1.pdf",
+          visto: false,
+          detalle: [
+            {
+              url: "https://www.youtube.com/embed/zpOULjyy-n8?rel=0", tipo: 0, visto: false
+            },
+            {
+              url: "https://www.um.es/docencia/barzana/DAWEB/Lenguaje-de-programacion-JavaScript-1.pdf", tipo: 1, visto: false
+            },
+            {
+              url: "https://camsolutionsgt.blogspot.com/", tipo: 2, visto: false
+            },
+            {
+              url: "https://www.lawebdelprogramador.com/pdf/files/1532804764_manual-javascript-19.jpg", tipo: 3, visto: false
+            },
+            {
+              url: "https://www.youtube.com/watch?v=Sj-KJdE1bNY", tipo: 0, visto: false
+            },
+          ],
         },
         {
           codigoContenido: 3,
           nombreContenido: "Run to Completion and the Event Loop",
-          videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI"
+          videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+          visto: false
         },
         {
           codigoContenido: 4,
           nombreContenido: "Summary",
-          videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI"
+          videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+          visto: false
         }
         ],
         videoUrlContenido: ""
@@ -100,14 +236,38 @@ export class CursosComponent implements OnInit {
         codigoContenido: 2, refTarget: '#tema2', refNombre: 'tema2', documento: "no hay",
         nombreContenido: "Introducing Promises",
         subTemas: [
-          { codigoContenido: 1, nombreContenido: "Basic Usage", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 2, nombreContenido: "Multiple Consumers", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 3, nombreContenido: "Promise States", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 4, nombreContenido: "Chaining Promises", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 5, nombreContenido: "Callback Execution Order", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 6, nombreContenido: "Basic Error Propagation", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 7, nombreContenido: "The Promise API", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 8, nombreContenido: "Summary", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
+          {
+            codigoContenido: 1, nombreContenido: "Basic Usage", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 2, nombreContenido: "Multiple Consumers", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 3, nombreContenido: "Promise States", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 4, nombreContenido: "Chaining Promises", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 5, nombreContenido: "Callback Execution Order", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 6, nombreContenido: "Basic Error Propagation", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 7, nombreContenido: "The Promise API", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 8, nombreContenido: "Summary", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
         ],
         videoUrlContenido: "https://www.youtube.com/watch?v=ye21CYdhm1s"
       },
@@ -115,13 +275,34 @@ export class CursosComponent implements OnInit {
         codigoContenido: 3, refTarget: '#tema3', refNombre: 'tema3', documento: "no hay",
         nombreContenido: "Working with Standard Promises",
         subTemas: [
-          { codigoContenido: 1, nombreContenido: "The Async Ripple Effect", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 2, nombreContenido: "Conditional Logic", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 3, nombreContenido: "Parallel Execution", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 4, nombreContenido: "Sequential Execution Using Loops or Recursion", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 5, nombreContenido: "Managing Latency", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 6, nombreContenido: "Functional Composition", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
-          { codigoContenido: 7, nombreContenido: "Summary", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI" },
+          {
+            codigoContenido: 1, nombreContenido: "The Async Ripple Effect", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 2, nombreContenido: "Conditional Logic", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 3, nombreContenido: "Parallel Execution", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 4, nombreContenido: "Sequential Execution Using Loops or Recursion", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 5, nombreContenido: "Managing Latency", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 6, nombreContenido: "Functional Composition", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
+          {
+            codigoContenido: 7, nombreContenido: "Summary", videoUrlContenido: "https://www.youtube.com/watch?v=PY_pZNjJOPI",
+            visto: false
+          },
         ],
         videoUrlContenido: "https://www.youtube.com/watch?v=ye21CYdhm1s"
       }],
@@ -133,7 +314,7 @@ export class CursosComponent implements OnInit {
       nombreCurso: "Javascript Avanzado",
       $id: "-Ld9_yln2z7iF78EPqqr",
     }
-
+    this.leccionActual();
 
   }
 
