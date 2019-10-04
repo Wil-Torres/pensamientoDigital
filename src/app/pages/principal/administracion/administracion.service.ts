4550266b79b284eb2a6d582547a81a0459b4911c
Wtorres <wtorres@cads.com.gt>
2019-10-04 17:12:16 -0600

    Ajustes en grado, seccion y carrera

--- src/app/pages/principal/administracion/administracion.service.ts
+++ src/app/pages/principal/administracion/administracion.service.ts
@@ -1,3 +1,4 @@
+const tipoCiclos = [{ tipo: 'Anual', id: 1 }, { tipo: 'Semestral', id: 2 }, { tipo: 'Trimestral', id: 3 }, { tipo: 'Bimestral', id: 4 }]
 export interface Ciclo {
   id: string;
   ciclo: number;
@@ -6,17 +7,27 @@ export interface Ciclo {
   usuarioId: string;
 }
 
-export interface gradoSeccion{
+export interface SeccionArray extends Array<any> {
+  [index: number]: string;
+}
+
+export interface gradoSeccion {
   id: string;
   ciclo: string;
   estado: string;
-  carrera: string;
-  grado: string;
-  seccion: string;
+  carreraId: string;
+  carrera: Carrera;
+  gradoId: string;
+  grado: Grado;
+  seccion: SeccionArray;
   fechaCreacion: Date;
   usuarioId: string;
 }
 
+export interface Grado {
+  id: number;
+  descripcion: string;
+}
 export interface Carrera {
   id: string;
   usuarioId: string;
@@ -34,28 +45,35 @@ import { UserInfo } from 'src/app/interfaces/login';
   providedIn: 'root'
 })
 export class AdministracionService {
-  
+
   usuarioInfo: UserInfo;
-  private _ciclos : AngularFirestoreCollection<Ciclo>;
-  private _carreras : AngularFirestoreCollection<Carrera>;
+  private _ciclos: AngularFirestoreCollection<Ciclo>;
+  private _carreras: AngularFirestoreCollection<Carrera>;
 
+  private _grados: AngularFirestoreCollection<gradoSeccion>;
 
-  public get carreras() : AngularFirestoreCollection<Carrera> {
+  public get grados(): AngularFirestoreCollection<gradoSeccion> {
+    return this._grados;
+  }
+  public set grados(v: AngularFirestoreCollection<gradoSeccion>) {
+    this._grados = v;
+  }
+  public get carreras(): AngularFirestoreCollection<Carrera> {
     return this._carreras;
   }
-  public set carreras(v : AngularFirestoreCollection<Carrera>) {
+  public set carreras(v: AngularFirestoreCollection<Carrera>) {
     this._carreras = v;
   }
-  
-  public get ciclos() : AngularFirestoreCollection<Ciclo> {
+
+  public get ciclos(): AngularFirestoreCollection<Ciclo> {
     return this._ciclos;
   }
-  public set ciclos(v : AngularFirestoreCollection<Ciclo>) {
+  public set ciclos(v: AngularFirestoreCollection<Ciclo>) {
     this._ciclos = v;
   }
-  
 
-  constructor(private afs: AngularFirestore, private srvAuth: AuthService) { 
+
+  constructor(private afs: AngularFirestore, private srvAuth: AuthService) {
     this.srvAuth.userInfo.subscribe(usuario => {
       this.usuarioInfo = usuario;
     })
@@ -65,54 +83,65 @@ export class AdministracionService {
   ********************************** CICLOS *************************************
   *******************************************************************************
   */
-  getCiclos(){
+  getCiclos() {
     this.ciclos = this.afs.collection('ciclos')
-    return  this.ciclos.valueChanges();
+    return this.ciclos.valueChanges();
   }
-  getCiclo(id: string){
+  getCiclo(id: string) {
     return this.afs.collection('ciclos').doc(id).valueChanges();
   }
-  postCiclo(obj:Ciclo){
+  postCiclo(obj: Ciclo) {
     return this.afs.collection('ciclos').add(obj)
   }
-  putCiclo(obj:Ciclo){
+  putCiclo(obj: Ciclo) {
     return this.afs.collection('ciclos').doc(obj.id).update(obj)
   }
-  deleteCiclo(obj:Ciclo){
+  deleteCiclo(obj: Ciclo) {
     return this.afs.collection('ciclos').doc(obj.id).delete();
   }
-  
+
 
   /* ****************************************************************************
   ********************************** CARRERAS ***********************************
   *******************************************************************************
   */
- getCarreras(){
-  this.carreras = this.afs.collection('carreras')
-  return  this.carreras.valueChanges();
- }
- getCarrera(id: string){
-  return this.afs.collection('carreras').doc(id).valueChanges();
- }
- postCarrera(obj: Carrera){
-  return this.afs.collection('carreras').add(obj)
- }
- putCarrera(obj: Carrera){
-  return this.afs.collection('carreras').doc(obj.id).update(obj)
- }
- deleteCarrera(obj:Carrera){
-  return this.afs.collection('carreras').doc(obj.id).delete();
- }
+  getCarreras() {
+    this.carreras = this.afs.collection('carreras')
+    return this.carreras.valueChanges();
+  }
+  getCarrera(id: string) {
+    return this.afs.collection('carreras').doc(id).valueChanges();
+  }
+  postCarrera(obj: Carrera) {
+    return this.afs.collection('carreras').add(obj)
+  }
+  putCarrera(obj: Carrera) {
+    return this.afs.collection('carreras').doc(obj.id).update(obj)
+  }
+  deleteCarrera(obj: Carrera) {
+    return this.afs.collection('carreras').doc(obj.id).delete();
+  }
 
   /* ****************************************************************************
   **************************** GRADOS SECICIONES ********************************
   *******************************************************************************
   */
- getGradosSecciones(){}
- getGradoSeccion(){}
- postGradoSeccion(){}
- putGradoSeccion(){}
- deleteGradoSeccion(){}
+  getGradosSecciones() {
+    this.grados = this.afs.collection('grados')
+    return this.grados.valueChanges();
+  }
+  getGradoSeccion(id: string) {
+    return this.afs.collection('grados').doc(id).valueChanges();
+  }
+  postGradoSeccion(obj: gradoSeccion) {
+    return this.afs.collection('grados').add(obj)
+  }
+  putGradoSeccion(obj: gradoSeccion) {
+    return this.afs.collection('grados').doc(obj.id).update(obj)
+  }
+  deleteGradoSeccion(obj: gradoSeccion) {
+    return this.afs.collection('grados').doc(obj.id).delete();
+  }
 
   /* ****************************************************************************
   **********************************  *************************************
--- src/app/pages/principal/administracion/grados-secciones/grados-secciones.component.ts
+++ src/app/pages/principal/administracion/grados-secciones/grados-secciones.component.ts
@@ -1,15 +1,130 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { gradoSeccion, AdministracionService, Carrera } from '../administracion.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert';
+import { isNil } from 'lodash';
 
 @Component({
   selector: 'app-grados-secciones',
   templateUrl: './grados-secciones.component.html',
-  styles: []
+  styles: [`
+  .w-40 {
+		width: 40%!important;
+  }
+  `]
 })
 export class GradosSeccionesComponent implements OnInit {
+  modalRef: BsModalRef | null;
+  gradoEscolar: any = {};
+  objCarreras: Carrera[];
+  private _objGrados: gradoSeccion[] = [];
+  private _cicloId: string = this.Aroute.snapshot.paramMap.get('id');
+  public get objGrados(): gradoSeccion[] {
+    return this._objGrados;
+  }
+  public set objGrados(v: gradoSeccion[]) {
+    this._objGrados = v;
+  }
 
-  constructor() { }
+
+  constructor(private modalService: BsModalService, private srvAdmin: AdministracionService,
+    private router: Router, private Aroute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.srvAdmin.getGradosSecciones().subscribe(grados => {
+      this.objGrados = grados;
+    })
+    this.srvAdmin.getCarreras().subscribe(carreras => {
+      this.objCarreras = carreras;
+    })
+  }
+
+  agregarGrado() {
+    let valida = this.validarGrado();
+    if (valida.error) {
+      swal('Error en validación', valida.mensaje, 'warning');
+      return;
+    }
+    this.asignacion().then((nuevaGrado: gradoSeccion) => {
+      if (isNil(nuevaGrado.id)) {
+        this.srvAdmin.postGradoSeccion(nuevaGrado).then(resp => {
+          this.gradoEscolar = {};
+          resp.update({ id: resp.id }).then(gradoNew => {
+            swal("Grado y Seccion Creados", 'exitosamente', 'success');
+          })
+        })
+      } else {
+        this.srvAdmin.putGradoSeccion(nuevaGrado).then(resp => {
+          swal("Grado y Seccion Actualizado", 'exitosamente', 'success');
+        })
+      }
+
+    })
+
+
+  }
+
+  validarGrado() {
+    let respuesta = { error: false, mensaje: '' }
+    let existeGrado = this.objGrados.find(elem => {
+      return ((this.gradoEscolar.gradoId === elem.gradoId) && (this.gradoEscolar.carreraId === elem.carreraId))
+    })
+    let existeSeccion
+    if (existeGrado) {
+      existeSeccion = existeGrado.seccion.find(elem => {
+        return elem === this.gradoEscolar.seccion;
+      })
+    }
+
+    if (existeSeccion) {
+      return respuesta = { error: true, mensaje: ' La sección que desea ingresar ya existe' };
+    }
+
+    return respuesta;
+  }
+
+  asignacion() {
+
+    return new Promise((resolve) => {
+
+      let id = null;
+      let existeCarrera = this.objGrados.find(elem => {
+        return ((elem.carreraId === this.gradoEscolar.carreraId) && (elem.gradoId === this.gradoEscolar.gradoId));
+      })
+      if (existeCarrera) {
+        id = existeCarrera.id;
+        this.srvAdmin.getGradoSeccion(id).subscribe((elem: any) => {
+          elem.seccion.push(this.gradoEscolar.seccion);
+          resolve(elem);
+        })
+      } else {
+        resolve({
+          id: null,
+          ciclo: this._cicloId,
+          estado: 'A',
+          carreraId: this.gradoEscolar.carreraId,
+          carrera: this.gradoEscolar.carrera,
+          gradoId: this.gradoEscolar.gradoId,
+          seccion: [this.gradoEscolar.seccion],
+          fechaCreacion: new Date(),
+          usuarioId: this.srvAdmin.usuarioInfo.uid,
+        })
+      }
+
+    })
+
+  }
+
+  seleccionarCarrera(id: string) {
+    console.log(id)
+    let x = this.objCarreras.find(elem => {
+      return id === elem.id
+    })
+
+    if (x) {
+      this.gradoEscolar.carrera = x;
+    }
   }
 
 }
